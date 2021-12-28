357744f687159316eb2af836654d057890a37f42
ranaahmedali2 <ranaahmedali2@hotmail.com>
2021-12-28 05:44:19 +0500

    Updates

---
+++ precache-manifest.f227510b832a0b767a2f2f4e97b29eb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f994146c70fc64f98a6a1cc131d230c",
+    "url": "/DApp-Exchange/index.html"
+  },
+  {
+    "revision": "ef37a7866fb20fff8e05",
+    "url": "/DApp-Exchange/static/css/2.f2635a7b.chunk.css"
+  },
+  {
+    "revision": "13a7dd3f9e5f4ecabccc",
+    "url": "/DApp-Exchange/static/css/main.94086239.chunk.css"
+  },
+  {
+    "revision": "ef37a7866fb20fff8e05",
+    "url": "/DApp-Exchange/static/js/2.9835f860.chunk.js"
+  },
+  {
+    "revision": "7fd9a006e1625008af9e7e80bb98b59a",
+    "url": "/DApp-Exchange/static/js/2.9835f860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a7dd3f9e5f4ecabccc",
+    "url": "/DApp-Exchange/static/js/main.cf0e1b66.chunk.js"
+  },
+  {
+    "revision": "323a436ab1b8e884be1d",
+    "url": "/DApp-Exchange/static/js/runtime-main.7bfc99e7.js"
+  }
+]);
